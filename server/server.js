0181a9cedd76347efc0d90a4847893c6c593adf2
huntrreed <146265520+huntrreed@users.noreply.github.com>
2024-04-04 03:40:29 -0600

    fixed routes leftover in server.js

--- server/server.js
+++ server/server.js
@@ -1,43 +1,39 @@
+// Importing modules
 const express = require('express');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
+const db = require('./config/connection');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
+// GraphQL will replace existing routes
+// const routes = require('./routes'); 
 
+// Creating an express server
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
-const startApolloServer = async () => {
+async function startApolloServer() {
   await server.start();
+  server.applyMiddleware({ app });
+}
 
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+startApolloServer();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-  
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
 
-  // Use Apollo Server as middleware to the Express app
-  app.use(expressMiddleware(server, {
-    path: '/graphql',
-  }));
-
- // Wait for the db connection before starting the Express server
- db.once('open', () => {
+db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
-};
-
-startApolloServer();
